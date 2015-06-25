edda9f5f611d8b2c8379a62218a5093da6c85465
renaud <renaud@renaud-Lenovo-Yoga-2-Pro>
2015-06-25 19:02:46 +0200

    Merge origin/master
    Conflicts:
    	README.md
    
    Implementation of Network Codes on a simple client
    
    removal of badly placed server files.

--- src/org/cri/swarm/client/Client.java
+++ src/org/cri/swarm/client/Client.java
@@ -5,6 +5,8 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,32 +16,150 @@ import java.util.logging.Logger;
  * <arthur.besnard@ovh.fr>
  */
 public class Client {
+
     private final SocketChannel sc;
     private static final Charset UTF8_CS = Charset.forName("UTF8");
+    ArrayList<Byte> codeSend = new ArrayList<>();
+    ArrayList<Double> xPlayer = new ArrayList<>();
+    ArrayList<Double> yPlayer = new ArrayList<>();
+    ArrayList<Double> zPlayer = new ArrayList<>();
+
+    ArrayList<Integer> listId = new ArrayList<>();
+
+    private int nPlayers = 0;
+    private int clientId = 0;
+    private double X;
+    private double Y;
+    private double Z;
+
+    private final ByteBuffer codeReadBuffer = ByteBuffer.allocate(Byte.BYTES + Double.BYTES * 3);
+    private final ByteBuffer gameInfoBuffer = ByteBuffer.allocate(Integer.BYTES * 2);
+    private final ByteBuffer playerIdBuffer = ByteBuffer.allocate(Integer.BYTES);
+    private ByteBuffer listIdBuffer;
+    private ByteBuffer listPositionBuffer;
+    Random r = new Random();
+
     private Client(String host, int port) throws IOException {
-        sc=SocketChannel.open(new InetSocketAddress(host, port));
+        sc = SocketChannel.open(new InetSocketAddress(host, port));
     }
-    
-    public void launch(String name) throws IOException{
+
+    public void launch(String name) throws IOException {
+        X = r.nextDouble();
+        Y = r.nextDouble();
+        Z = r.nextDouble();
         register(name);
         readListIdPlayer();
-        while(!Thread.interrupted()){
-            sendPosition();
-            receivePositions();
+        while (!Thread.interrupted()) {
+            codeWrite();
+            send();
+            receive();
         }
     }
-    
-    private void register(String name) throws IOException{
+
+    private void register(String name) throws IOException {
         ByteBuffer nameBuff = UTF8_CS.encode(name);
         ByteBuffer buff = ByteBuffer.allocate(Byte.BYTES + Integer.BYTES + nameBuff.remaining());
-        buff.put((byte)10);
+        buff.put((byte) 10);
         buff.putInt(nameBuff.remaining());
         buff.put(nameBuff);
         buff.flip();
         sc.write(buff);
     }
-    
-    
+
+    private void readListIdPlayer() throws IOException {
+        sc.read(codeReadBuffer);
+        if (!codeReadBuffer.hasRemaining()) {
+            codeReadBuffer.flip();
+            if (codeReadBuffer.get() == (byte) 11) {
+                sc.read(gameInfoBuffer);
+                if (!gameInfoBuffer.hasRemaining()) {
+                    nPlayers = gameInfoBuffer.getInt();
+                    clientId = gameInfoBuffer.getInt();
+
+                }
+            }
+            listIdBuffer = ByteBuffer.allocate(nPlayers * Integer.BYTES);
+            sc.read(listIdBuffer);
+            if (!listIdBuffer.hasRemaining()) {
+                listIdBuffer.flip();
+                for (int i = 0; i < nPlayers; i++) {
+                    listId.add(listIdBuffer.getInt());
+                }
+
+            }
+            gameInfoBuffer.clear();
+            listIdBuffer.clear();
+            codeReadBuffer.clear();
+        }
+
+    }
+
+    private void send() throws IOException {
+
+        for (byte code : codeSend) {
+            switch (code) {
+                case (byte) 20:
+                    ByteBuffer buff = ByteBuffer.allocate(Byte.BYTES + Double.BYTES * 3);
+                    buff.put((byte) 20);
+                    buff.putDouble(X);
+                    buff.putDouble(Y);
+                    buff.putDouble(Z);
+                    buff.flip();
+                    sc.write(buff);
+
+            }
+            codeSend.remove(0);
+        }
+    }
+
+    private void receive() throws IOException {
+        sc.read(codeReadBuffer);
+        if (!codeReadBuffer.hasRemaining()) {
+            codeReadBuffer.flip();
+            switch (codeReadBuffer.get()) {
+                case (byte) 21:
+                    listPositionBuffer = ByteBuffer.allocate(nPlayers * Double.BYTES * 3);
+                    sc.read(listPositionBuffer);
+                    if (!listPositionBuffer.hasRemaining()) {
+                        listPositionBuffer.flip();
+
+                        for (int playerId : listId) {
+                            xPlayer.add(listPositionBuffer.getDouble());
+                            yPlayer.add(listPositionBuffer.getDouble());
+                            zPlayer.add(listPositionBuffer.getDouble());
+                        }
+                    }
+                    listPositionBuffer.clear();
+                case (byte) 31:
+                    System.exit(0);
+                    
+                case (byte) 33:
+                    sc.read(playerIdBuffer);
+                    if (!playerIdBuffer.hasRemaining()) {
+                        playerIdBuffer.flip();
+                        listId.add(playerIdBuffer.getInt());
+                        nPlayers++;
+                    }
+                    playerIdBuffer.clear();
+                    
+                case (byte) 35:
+                    sc.read(playerIdBuffer);
+                    if (!playerIdBuffer.hasRemaining()) {
+                        playerIdBuffer.flip();
+                        listId.remove(playerIdBuffer.getInt());
+                        xPlayer.remove(playerIdBuffer.getInt());
+                        yPlayer.remove(playerIdBuffer.getInt());
+                        zPlayer.remove(playerIdBuffer.getInt());
+                        
+                        nPlayers--;
+                    }
+                    playerIdBuffer.clear();
+            }
+            listPositionBuffer.clear();
+            codeReadBuffer.clear();
+        }
+    }
+
     public static void main(String args[]) {
         String host;
         int port;
@@ -56,4 +176,10 @@ public class Client {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, "Server Connection Time Out", ex);
         }
     }
+
+    private void codeWrite() {
+        codeSend.add((byte) 20);
+
+    }
+
 }
--- src/org/cri/swarm/server/ReadOp.java
+++
@@ -1,8 +0,0 @@
-package org.cri.swarm.server;
-
-import java.io.IOException;
-
-interface ReadOp {
-    void read() throws IOException;
-    boolean isFinished();
-}
